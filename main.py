2686476809da24f93d165e070a5f2ddaa7829fb1
chaoukat99 <chawkatomar@gmail.com>
2025-06-12 20:05:55 +0100

    Adding an age condition

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ print("Hello Guys")
 
 
 
-for i in range(20):
-    print("Hello Gomycode "+str(i+1))
+# for i in range(20):
+#     print("Hello Gomycode "+str(i+1))
+
+
+
+age=15
+
+
+if age>18:
+    print("Adulte")
+else: 
+    print("Mineur ")
